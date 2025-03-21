81ce7ba479f7a377c7fd5970bcd1efa027f70eeb
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-03-21 21:19:26 +0300

    Create ApplicationDbContext.cs

---
+++ EvaluationCrm/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using EvaluationCrm.Models.entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace EvaluationCrm.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    
+    public DbSet<Role> Roles { get; set; }
+    
+    //public DbSet<Role> Roles { get; set; }
+    // Ajoute d'autres DbSets pour d'autres entités
+}
