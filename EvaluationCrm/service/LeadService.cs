0bf04c48dae4267118fea9fb8bf4a170e71f5628
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-03-23 12:20:34 +0300

    add lead feature edit

---
+++ EvaluationCrm/service/LeadService.cs
@@ -0,0 +1,25 @@
+﻿using EvaluationCrm.Models.entity;
+
+namespace EvaluationCrm.service;
+
+public class LeadService
+{
+    private readonly HttpClient _httpClient;
+    
+    public LeadService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    
+    public List<Lead>? GetLeads()
+    {
+        var content =  _httpClient.GetFromJsonAsync<List<Lead>>("http://localhost:8080/api/expense/lead").Result;
+        return content;
+    }
+
+    public void DeleteLead(Int16 leadId)
+    {
+        _httpClient.DeleteAsync($"http://localhost:8080/api/expense/delete-lead?leadId={leadId}");
+    }
+    
+}
