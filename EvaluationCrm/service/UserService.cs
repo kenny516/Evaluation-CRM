915a2e8d024bc318472fd2fe7ef831599db24bff
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-03-24 14:22:13 +0300

    Update UserService.cs

--- EvaluationCrm/service/UserService.cs
+++ EvaluationCrm/service/UserService.cs
@@ -13,6 +13,31 @@ public class UserService
         _httpClient = httpClient;
     }
 
+    public async Task<User?> Login(string email, string password)
+    {
+        var formData = new Dictionary<string, string>
+        {
+            { "email", email },
+            { "password", password }
+        };
+        
+        var content = new FormUrlEncodedContent(formData);
+
+        // Envoyer la requête POST
+        var response = await _httpClient.PostAsync("http://localhost:8080/api/user/login", content);
+
+        // Vérifier si la réponse est réussie
+        if (response.IsSuccessStatusCode)
+        {
+            var res = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<User>(res);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
     public async Task<User?> GetUserByEmail(string email)
     {
         try
