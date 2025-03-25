0f67e8342e0bbcdfa785c19939145458f8eedc7b
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-03-25 21:19:49 +0300

    Add Dashboard model and service; refactor DashboardController to use new service

---
+++ EvaluationCrm/Models/entity/Dashboard.cs
@@ -0,0 +1,11 @@
+﻿namespace EvaluationCrm.Models.entity;
+
+public class Dashboard
+{
+    public IDictionary<string, double> TicketExpensesByCustomer { get; set; }
+    public IDictionary<string, double> LeadExpensesByCustomer { get; set; }
+    public IDictionary<string, double> BudgetByCustomer { get; set; }
+    public double TotalTicketExpenses { get; set; }
+    public double TotalLeadExpenses { get; set; }
+    public double TotalBudget { get; set; }
+}
